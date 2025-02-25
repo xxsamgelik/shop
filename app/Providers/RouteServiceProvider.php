1f04c049597db63e3630dc6a93cf0db587075843
Alfagrad <alfagrad@alfagrad.com>
2025-02-25 20:48:01 +0300

    init

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -5,8 +5,10 @@ namespace App\Providers;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpFoundation\Response as ResponseAlias;
 
 class RouteServiceProvider extends ServiceProvider
 {
@@ -24,6 +26,14 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        RateLimiter::for('global', function (Request $request) {
+            return Limit::perMinute(500)->by($request->user()?->id ?: $request->ip())
+                ->response(function (Request $request,array $headers) {
+                    return response('Take it easy', ResponseAlias::HTTP_TOO_MANY_REQUESTS);
+                })
+                ;
+        });
+
         RateLimiter::for('api', function (Request $request) {
             return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
         });
